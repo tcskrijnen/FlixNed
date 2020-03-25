417266ea52736f5d7ffa3d681778aa217ebe2743
tcskrijnen <tim.krijnen9@gmail.com>
2020-03-25 10:39:45 +0100

    Auth service set-up

--- FlixNed_Service/auth/src/main/java/com/flixned/auth/AuthApplication.java
+++ FlixNed_Service/auth/src/main/java/com/flixned/auth/AuthApplication.java
@@ -1,16 +1,14 @@
-package com.gateway;
+package com.flixned.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZuulProxy
-public class GatewayApplication {
-
+public class AuthApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+
+        SpringApplication.run(AuthApplication.class, args);
     }
 }
--- FlixNed_Service/service_registration/src/main/java/com/flixned/serviceregistration/ServiceRegistrationApplication.java
+++ FlixNed_Service/service_registration/src/main/java/com/flixned/serviceregistration/ServiceRegistrationApplication.java
@@ -1,4 +1,4 @@
-package com.serviceregistration;
+package com.flixned.serviceregistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
