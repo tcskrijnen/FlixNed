7ac550a1cec53d1b05eafac614c7b14b5895cfd0
tcskrijnen <tim.krijnen9@gmail.com>
2020-04-17 11:00:49 +0200

    utils added, exceptions added, error handling customer registration

--- FlixNed_Service/customer_service/src/main/java/com/flixned/customerservice/services/CustomerService.java
+++ FlixNed_Service/customer_service/src/main/java/com/flixned/customerservice/services/CustomerService.java
@@ -4,7 +4,10 @@ import com.flixned.customerservice.common.dto.RegisterDTO;
 import com.flixned.customerservice.common.models.Customer;
 import com.flixned.customerservice.common.models.User;
 import com.flixned.customerservice.common.utils.AuthenticationUtils;
+import com.flixned.customerservice.common.utils.RandomString;
+import com.flixned.customerservice.exceptions.DatabaseException;
 import com.flixned.customerservice.repositories.CustomerRepository;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import static com.flixned.customerservice.common.security.UserRole.ADMIN;
@@ -25,11 +28,16 @@ public class CustomerService {
 
     public String Registration(RegisterDTO register) {
 
-        User newCustomer = new Customer("12345", register.getEmail(), new AuthenticationUtils().encode(register.getPassword()), true, true, true, true, USER.getGrantedAuthorities());
+        RandomString rdm = new RandomString();
 
-        customerRepository.save(newCustomer);
+        User newCustomer = new Customer(rdm.getAlphaNumericString(10), register.getEmail(), new AuthenticationUtils().encode(register.getPassword()), true, true, true, true, USER.getGrantedAuthorities());
 
-        return "saved";
+        try{
+            customerRepository.save(newCustomer);
 
+            return "saved";
+        }catch (DatabaseException ex){
+            throw new DatabaseException("customer not saved");
+        }
     }
 }
