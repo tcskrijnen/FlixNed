b0890e12578d049a786a72f1964758813835fd70
tcskrijnen <tim.krijnen9@gmail.com>
2020-05-15 15:42:38 +0200

    streaming service module

--- customer_service/src/main/java/com/flixned/customerservice/controllers/RestURIConstant.java
+++ customer_service/src/main/java/com/flixned/customerservice/controllers/RestURIConstant.java
@@ -2,7 +2,7 @@ package com.flixned.customerservice.controllers;
 
 public class RestURIConstant {
 
-    public static final String current = "/user/current";
+    public static final String current = "/current";
     public static final String register = "/register";
     public static final String getUserByEmail = "/user/getUserByEmail";
     public static final String getCustomerByEmail = "/user/getCustomerByEmail";
