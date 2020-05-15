bd37acbeabd37dd2aeb92f3660cdb96df1be2287
tcskrijnen <tim.krijnen9@gmail.com>
2020-05-15 14:46:51 +0200

    TokenConfig edit

--- customer_service/src/main/java/com/flixned/customerservice/controllers/RestURIConstant.java
+++ customer_service/src/main/java/com/flixned/customerservice/controllers/RestURIConstant.java
@@ -3,7 +3,7 @@ package com.flixned.customerservice.controllers;
 public class RestURIConstant {
 
     public static final String current = "/user/current";
-    public static final String register = "/user/register";
+    public static final String register = "/register";
     public static final String getUserByEmail = "/user/getUserByEmail";
     public static final String getCustomerByEmail = "/user/getCustomerByEmail";
     public static final String test = "/user/test";
--- gateway/src/main/java/com/flixned/gateway/security/SecurityTokenConfig.java
+++ gateway/src/main/java/com/flixned/gateway/security/SecurityTokenConfig.java
@@ -33,6 +33,7 @@ public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 // allow all who are accessing "auth" service
                 .antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
+                .antMatchers("/customer/**").permitAll()
                 // must be an admin if trying to access admin area (authentication is also required here)
                 // Any other request must be authenticated
                 .anyRequest().authenticated();
