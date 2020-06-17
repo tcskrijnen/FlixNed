f86c653c97818a6b841b59e547dd3fed7031f9c4
tcskrijnen <tim.krijnen9@gmail.com>
2020-06-17 10:09:24 +0200

    playlistDTO

--- content_service/src/main/java/com/flixned/contentservice/services/PlaylistService.java
+++ content_service/src/main/java/com/flixned/contentservice/services/PlaylistService.java
@@ -1,17 +1,29 @@
 package com.flixned.contentservice.services;
 
+import com.flixned.contentservice.common.dto.PlaylistDTO;
+import com.flixned.contentservice.common.models.Movie;
 import com.flixned.contentservice.common.models.Playlist;
+import com.flixned.contentservice.common.models.Serie;
+import com.flixned.contentservice.repositories.MovieRepository;
 import com.flixned.contentservice.repositories.PlaylistRepository;
+import com.flixned.contentservice.repositories.SerieRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
 public class PlaylistService {
+
+    private final SerieRepository serieRepository;
+    private final MovieRepository movieRepository;
     private final PlaylistRepository playlistRepository;
 
-    public PlaylistService(PlaylistRepository playlistRepository) {
+    public PlaylistService(SerieRepository serieRepository, MovieRepository movieRepository, PlaylistRepository playlistRepository) {
+        this.serieRepository = serieRepository;
+        this.movieRepository = movieRepository;
         this.playlistRepository = playlistRepository;
     }
 
@@ -31,8 +43,23 @@ public class PlaylistService {
         playlistRepository.save(playlist);
     }
 
-    public Playlist getPlaylist(int userId){
-        return playlistRepository.findPlaylistByUserId(userId);
+    public PlaylistDTO getPlaylist(int userId){
+        Playlist playlist = playlistRepository.findPlaylistByUserId(userId);
+
+        List<Serie> series = new ArrayList<>();
+        List<Movie> movies = new ArrayList<>();
+
+        for (String serieId: playlist.getSerieList()) {
+            Serie serie = serieRepository.getSerieBySerieId(serieId);
+            series.add(serie);
+        }
+
+        for (String movieId: playlist.getMovieList()) {
+            Movie movie = movieRepository.getMovieByMovieId(movieId);
+            movies.add(movie);
+        }
+
+        return new PlaylistDTO(series, movies);
     }
 
     public void createPlaylist(int userId){
