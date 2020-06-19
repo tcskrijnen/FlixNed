a9964ae345bbb4191d4dbf7430682eef99f9effe
tcskrijnen <tim.krijnen9@gmail.com>
2020-06-19 14:05:10 +0200

    fixes for live development

--- auth/src/main/java/com/flixned/auth/AuthApplication.java
+++ auth/src/main/java/com/flixned/auth/AuthApplication.java
@@ -21,7 +21,6 @@ public class AuthApplication {
 
         // Create a bean for restTemplate to call services
         @Bean
-        @LoadBalanced        // Load balance between service instances running at different ports.
         public RestTemplate restTemplate() {
             return new RestTemplate();
         }
